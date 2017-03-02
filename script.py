5a0cec17d27e36f82844fdc5fdc3854e24aeaece
martengooz <removed.due.to.reqruiter.spam@gooz.se>
2017-03-02 08:47:27 +0100

    Add filterrepo. Removes forks

--- script.py
+++ script.py
@@ -30,8 +30,26 @@ def checkRateLimit():
 	ratelimit = json.loads(requests.get(baseurl + "rate_limit", auth=(username, token)).text)
 	requestsleft = ratelimit['rate']['remaining']
 	resettime = ratelimit['rate']['reset']
-	if (requestsleft > 10):
+	if (requestsleft > 100):
 		pause.until(resettime + 10)
+	print requestsleft
 
+def filterRepos(repos):
+	# Remove forks
+	res = [repo for repo in repos if repo['fork'] == False]
 
-print repList()
+	# TODO: Remove all empty repos
+	#for repo in res:
+	#	r = requests.get(repo['url'],params=pageparam, auth=(username, token))
+	#	res = json.loads(r.text)
+	#res = [repo for repo in repos if repo['fork'] == False]
+
+	print "Found " + str(len(res)) + " repositories"
+
+	return 
+
+repos = repList()
+print filterRepos(repos)
+
+print 
+checkRateLimit()
