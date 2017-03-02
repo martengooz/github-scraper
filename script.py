e4441581f50754a6b270fdd9d810486bb7775ccf
martengooz <martengr@kth.se>
2017-03-02 07:49:57 +0100

    Add ratelimit

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 import requests
 from requests.auth import HTTPBasicAuth
-
+from random import randint
+import pause
+import json
+import time
 # Get token
 f = open("token","r") 
 username = f.readline()
@@ -9,7 +12,18 @@ f.close()
 
 # Urls
 baseurl = "https://api.github.com/"
+pageparam = {'since':randint(1, 3)}
+
+## Main Loop
+
+def reqList():
+	r = requests.get(baseurl + "repositories", auth=(username, token))
+	res = json.loads(r.text)
 
 
-r = requests.get(baseurl + "repositories", auth=(username, token))
-print r.text
+def checkRateLimit():
+	ratelimit = json.loads(requests.get(baseurl + "rate_limit", auth=(username, token)).text)
+	requestsleft = ratelimit['rate']['remaining']
+	resettime = ratelimit['rate']['reset']
+	if (requestsleft > 10):
+		pause.until(resettime + 10)
