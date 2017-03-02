94de323b57c45aa58d745ec6be41f9b20cba0acc
martengooz <martengr@kth.se>
2017-03-02 08:16:00 +0100

    Add replist, get 100 random repos

--- script.py
+++ script.py
@@ -12,18 +12,26 @@ f.close()
 
 # Urls
 baseurl = "https://api.github.com/"
-pageparam = {'since':randint(1, 3)}
 
-## Main Loop
+## Get a list of 100 random repos
+def repList():
+	pageparam = {	'since':randint(1, 83570000), 
+					'page':randint(1,5),
+					'per_page':100 } 
 
-def reqList():
-	r = requests.get(baseurl + "repositories", auth=(username, token))
+	r = requests.get(baseurl + "repositories",params=pageparam, auth=(username, token))
 	res = json.loads(r.text)
+	if res == []: # if we recieved an empty page
+		repList()
+	return res
 
-
+## Halt program until rate limit reset
 def checkRateLimit():
 	ratelimit = json.loads(requests.get(baseurl + "rate_limit", auth=(username, token)).text)
 	requestsleft = ratelimit['rate']['remaining']
 	resettime = ratelimit['rate']['reset']
 	if (requestsleft > 10):
 		pause.until(resettime + 10)
+
+
+print repList()
