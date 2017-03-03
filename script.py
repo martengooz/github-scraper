45b38e628902808c0c00628ce74c808acc83e13b
martengooz <martengr@kth.se>
2017-03-03 12:26:11 +0100

    Remove unneccesary info from repos

--- script.py
+++ script.py
@@ -14,8 +14,8 @@ f.close()
 baseurl = "https://api.github.com/"
 
 def writeJson(data, filename):
-	f = open(filename,"w") 
-	f.write(json.dumps(data))
+	f = open(filename,"a") 
+	f.write(json.dumps(data, indent=2))
 	f.close()
 
 
@@ -23,7 +23,7 @@ def writeJson(data, filename):
 def repList():
 	pageparam = {	'since':randint(1, 83570000), # Seems like this is near the upper limit of repo ids right now
 					'page':randint(1,5),
-					'per_page':25 } 
+					'per_page':100 } 
 
 	r = requests.get(baseurl + "repositories",params=pageparam, auth=(username, token))
 	res = json.loads(r.text)
@@ -31,7 +31,6 @@ def repList():
 		repList()
 	
 	return res
-
 ## Halt program until rate limit reset
 def checkRateLimit():
 	ratelimit = json.loads(requests.get(baseurl + "rate_limit", auth=(username, token)).text)
@@ -40,27 +39,41 @@ def checkRateLimit():
 	if (requestsleft < 100):
 		pause.until(resettime + 10)
 	print "Requests left: " + str(requestsleft)
-	
+
 
 def filterRepos(repos):
 	# Remove forks
-	res = [repo for repo in repos if repo['fork'] == False]
+	noforks = [repo for repo in repos if repo['fork'] == False]
 	nonempty = []
 	
-	# Remove all empty repos
-	for repo in res:
-		r = requests.get(repo['url'], auth=(username, token))
+	for repo in noforks:
+		#Remove unnesseary keys from norepo
+		keys = ["id","full_name","url"]
+		res = { key: repo[key] for key in keys }
+
+		# Get repo info
+		r = requests.get(res['url'], auth=(username, token))
 		repoinfo = json.loads(r.text)
+		
 		#Remove repos smaller than 500 bytes
 		if repoinfo['size'] > 500:
-			nonempty.append(repo)
+			#Remove unnesseary keys from repoinfo
+			keys = [ "size", "stargazers_count", "watchers_count", "language", "has_issues", "has_downloads", "has_pages", "forks_count", "mirror_url", "open_issues_count", "forks", "open_issues", "watchers", "network_count", "subscribers_count", "created_at", "updated_at", "pushed_at"]
+			notempty = { key: repoinfo[key] for key in keys }
+
+			# Merge noforks info with repos not empty
+			temp = res.copy()
+			temp.update(notempty)
+
+			nonempty.append(temp)
 
 	res = nonempty
+
 	print "Found " + str(len(res)) + " repositories"
 	return res
 
-repos = repList()
-filterRepos(repos)
+repos = filterRepos(repList())
+writeJson(repos, "repos")
 
 print 
 checkRateLimit()
